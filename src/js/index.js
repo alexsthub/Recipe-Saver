c2e4b26709591834d8c24ecbe5ebc83b8b87b4e0
Alex Tan <alextan785@gmail.com>
2019-11-03 15:23:42 -0800

    almost done with form

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
 'use strict';
 
-
+fetch('http://127.0.0.1:8080/src/jsonData.json')
+.then((data) => {
+  return data.json();
+})
+.catch((error) => {
+  console.log(error.message);
+})
+.then((data) => {
+  console.log(data);
+})
