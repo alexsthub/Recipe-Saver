202b62a4842afe831c6c8eb6b1dee1d76ad2cbc9
Jerry Lin <linj1064@gmail.com>
2019-12-03 21:52:37 -0800

    changed logo color to white

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ export default class Logo extends Component {
   render() {
     return (
       <div className="logo-container">
-        <img className="logo" alt="logo" src={require("../img/logo.png")} />
+        <img className="logo" alt="logo" src={require("../img/logo_white.png")} />
       </div>
     );
   }
